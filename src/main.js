37fd44e7ad509b57d49be98478ec22c3b5744029
ssul <younghwan.sul@wehuddling.com>
2022-06-28 13:52:14 +0900

    [feat] 통신 로직 추가
    [fix] input 수정

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/tailwind.css'
-import './index.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/tailwind.css";
+import "./index.css";
+import VueSweetalert2 from "vue-sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
 
-const app = createApp(App)
-
-app.use(router).mount('#app')
-
+const app = createApp(App);
+app.use(VueSweetalert2);
+app.use(router).mount("#app");
