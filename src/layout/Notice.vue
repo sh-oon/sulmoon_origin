e14a0f18697f273f888e8625c9e04a8e67987377
허윤정 <yj.wiju@gmail.com>
2022-06-28 13:26:18 +0900

    공지 수정하다가 말았어요 죄송티비

--- src/layout/Notice.vue
+++ src/layout/Notice.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="content xs:w-full drop-shadow-lg mt-4 flex flex-col">
     <div class="flex flex-col">
-      <h2 class="font-bold text-xl">안녕하세요 컨퍼런스 위원회입니다</h2>
-      <span class="text-sm flex">여러분의 성의에 힘입어 제1회 컨퍼런스를 개최할 수 있었습니다.</span>
-      <span class="text-sm flex">하지만 여건상 모든인원을 수용하기 힘들어 추첨을 통해 참가인원을 선정하게 되었습니다.</span>
-      <span class="text-sm flex">오프라인 15명, 온라인 49명으로 선별할 예정이며</span>
+      <h2 class="font-bold text-xl">안녕하세요, 컨퍼런스 위원회입니다.</h2>
+      <span class="text-sm flex">컨퍼런스 안내에 앞서 여러분들의 관심과 성원에 심심한 감사의 말씀을 전합니다.</span>
+      <span class="text-sm flex">기다려 주신 만큼 모든 인원을 수용하고 싶었으나 공간, 비용, 시간 등의 제약으로 추첨을 통하여 선별을 진행할 예정입니다. </span>
+      <span class="text-sm flex">아래 사항을 필히 숙지하신 뒤 신청 부탁드립니다.</span>
       <span class="text-sm flex">오프라인은 5000원, 온라인은 2500원의 참가비가 책정되어있습니다</span>
       <span>인원 선발은 오프라인은 오픈채팅방 참여도와 추첨을 통해 선발 예정이며,</span>
       <span>온라인은 완전 추첨으로 선발할 예정입니다.</span>
