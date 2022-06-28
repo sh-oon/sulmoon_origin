ad12475530f1e14199edca68b52a121507a1cd36
SungHoon <jshyh1@naver.com>
2022-06-28 15:47:11 +0900

    공지사항 수정(너무 야매같음..)

--- src/layout/Notice.vue
+++ src/layout/Notice.vue
@@ -3,33 +3,48 @@
     <div class="flex flex-col">
       <h2 class="font-bold text-xl">안녕하세요, 컨퍼런스 위원회입니다.</h2>
       <span class="text-sm flex">컨퍼런스 안내에 앞서 여러분들의 관심과 성원에 심심한 감사의 말씀을 전합니다.</span>
+      <br>
       <span class="text-sm flex">기다려 주신 만큼 모든 인원을 수용하고 싶었으나 공간, 비용, 시간 등의 제약으로 추첨을 통하여 선별을 진행할 예정입니다. </span>
       <span class="text-sm flex">아래 사항을 필히 숙지하신 뒤 신청 부탁드립니다.</span>
+      <br>
       <span class="text-sm flex">오프라인은 5000원, 온라인은 2500원의 참가비가 책정되어있습니다</span>
-      <span>인원 선발은 오프라인은 오픈채팅방 참여도와 추첨을 통해 선발 예정이며,</span>
-      <span>온라인은 완전 추첨으로 선발할 예정입니다.</span>
+      <span class="text-sm flex">기다려 주신 만큼 모든 인원을 수용하고 싶었으나 공간, 비용, 시간 등의 제약으로 추첨을 통하여 선별을 진행할 예정입니다. 아래 사항을 필히 숙지하신 뒤 신청 부탁드립니다.</span>
       <br>
       <span class="font-bold text-xl">참가확정문자 수신후 24시간 내에 미입금시 기회는 예비참가자에게 넘어갈 예정입니다</span>
       <span class="font-bold text-xl">입금기한 꼭 확인해 주세요</span>
       <br>
-      <span>중복제출은 불가하며 불이익이 있을 수 있습니다</span>
-      <span>장소 : 강남역 인근 (개별공지)/ 게더타운</span>
+      <span>오프라인 참가 안내</span>
+      <span class="pl-4">&bull; 최대 정원: 15명</span>
+      <span class="pl-4">&bull; 참가 비용: 5,000원</span>
+      <span class="pl-4">&bull; 선별 방식: 오픈채팅방 참여도 + 일부 추첨</span>
+      <span class="pl-4">&bull; 개최 장소: 강남역 인근(참가자 개별 공지)</span>
       <br>
+      <span>온라인 참가 안내</span>
+      <span class="pl-4">&bull; 최대 정원: 49명</span>
+      <span class="pl-4">&bull; 참가 비용: 2,500원</span>
+      <span class="pl-4">&bull; 선별 방식: 100% 추첨</span>
+      <span class="pl-4">&bull; 개최 장소: 게더타운(참가자 개별 공지)</span>
       <br>
       <span>환불 규정</span>
       <span class="pl-4">&bull; 행사일로부터 7일: 100%</span>
       <span class="pl-4">&bull; 행사일로부터 3~6일: 50%</span>
       <span class="pl-4">&bull; 행사일로부터 3일이내: 불가</span>
       <br>
+      <span class="font-bold">추가 공지사항</span>
+      <span class="pl-4 font-bold">&bull; 참가 확정문자 수신후 24시간 내 미입금시 참여기회는 다른 사람에게 양도됩니다.</span>
+      <span class="pl-4 font-bold">&bull; 중복 제출은 불가능 하며 이로 인한 불이익은 책임지지 않습니다.</span>
+      <span class="pl-4 font-bold">&bull; 컨퍼런스 내용에 대한 공식적인 공개는 없을 예정입니다.</span>
       <br>
+      <span>더욱 양질의 컨퍼런스를 위하여 사전 공지했던 바와 일부 달라진 점이 있습니다.</span>
+      <span>모두가 만족하는 컨퍼런스가 될 수 있도록 최선을 다하겠습니다.</span>
+      <span>더불어 컨퍼런스를 후원해 주신 분들과 단톡방 운영진분들께 감사의 말씀을 올립니다.</span>
       <br>
-      <span> * 컨퍼런스 내용은 현재 공개 예정이 없습니다</span>
+      <span>추가 문의 사항은 컨퍼런스 위원회에게 연락 주시면 성심성의껏 답변 드리겠습니다.</span>
     </div>
   </div>
 </template>
 
 <script setup>
-
 </script>
 
 <style scoped>
